ff442a830ab9e2c1eb595a5e261018c450394429
Evan Pickett <pickett7474@hotmail.com>
2020-04-13 15:11:44 -0700

    Update IntPair.java

--- IntPair.java
+++ IntPair.java
@@ -1,4 +1,8 @@
-
+/**
+ * IntPair
+ * Utility class used for making our lives easier
+ * Can hold an x and y value, which is easy to get from a chess board
+ */
 public class IntPair {
 	public int x;
 	public int y;
