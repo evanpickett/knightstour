19c4b356fb0f746bc01c9e6f31899a5b181d8e28
Evan Pickett <pickett7474@hotmail.com>
2020-04-14 14:09:53 -0700

    Update IntPair.java

--- IntPair.java
+++ IntPair.java
@@ -16,4 +16,31 @@ public class IntPair {
 		x = 0;
 		y = 0;
 	}
+	
+	public IntPair add(IntPair other) {
+		return new IntPair(x+other.x, y+other.y);
+	}
+	
+	@Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        //got this from: https://stackoverflow.com/questions/8180430/how-to-override-equals-method-in-java
+        if (!IntPair.class.isAssignableFrom(obj.getClass())) {
+            return false;
+        }
+        final IntPair other = (IntPair) obj;
+        if (this.x != other.x) {
+            return false;
+        }
+        if (this.y != other.y) {
+            return false;
+        }
+        return true;
+    }
+	@Override
+	public String toString() {
+		return "(" + x + ", " + y + ")";
+	}
 }
